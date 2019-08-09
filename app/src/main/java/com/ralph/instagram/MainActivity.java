440c3151971de87577dd015d46d08e3339887dac
ralph <placideralph@gmail.com>
2019-08-09 01:27:23 -0400

    building fragment home layout

--- app/src/main/java/com/ralph/instagram/MainActivity.java
+++ app/src/main/java/com/ralph/instagram/MainActivity.java
@@ -1,14 +1,70 @@
 package com.ralph.instagram;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.ralph.instagram.fragment.HomeFragment;
 
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //loading the default fragment
+        loadFragment(new HomeFragment());
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bnView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.ic_bnv_home);
+    }
+
+    public Boolean loadFragment(Fragment fragment){
+        if(fragment != null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragmentLayout, fragment)
+                    .commit();
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+
+        switch (menuItem.getItemId()) {
+            case R.id.ic_bnv_home:
+                fragment = new HomeFragment();
+                break;
+
+            case R.id.ic_bnv_explore:
+                //todo : add explore fragment
+                break;
+
+            case R.id.ic_bnv_post:
+                //todo : add post fragment
+                break;
+
+            case R.id.ic_bnv_like:
+                //todo : add like fragment
+                break;
+
+            case R.id.ic_bnv_mywall:
+                //todo : add like fragment
+                break;
+        }
+        return loadFragment(fragment);
     }
 }
---
+++ app/src/main/java/com/ralph/instagram/fragment/HomeFragment.java
@@ -0,0 +1,41 @@
+package com.ralph.instagram.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.ralph.instagram.R;
+
+public class HomeFragment extends Fragment {
+
+    RecyclerView rvInsta;
+    Toolbar toolbar;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        toolbar =view.findViewById(R.id.toolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        rvInsta = view.findViewById(R.id.rvInsta);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+}
